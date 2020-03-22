126ee46344594b7ff508c6779f779027fee6f987
wr1ttenyu <826543564@qq.com>
2020-03-22 21:11:40 +0800

    some stuff

--- project-archetype-test/src/test/java/wr1ttenyu/f1nal/study/project/archetype/test/kafka/KafkaProducerTest.java
+++ project-archetype-test/src/test/java/wr1ttenyu/f1nal/study/project/archetype/test/kafka/KafkaProducerTest.java
@@ -1,14 +1,13 @@
 package wr1ttenyu.f1nal.study.project.archetype.test.kafka;
 
-import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
-public class KafkaTest /*extends BaseTest */ {
+public class KafkaProducerTest {
 
     public static void main(String[] args) {
         Map<String, Object> connectInfo = new HashMap<>();
@@ -28,17 +27,31 @@ public class KafkaTest /*extends BaseTest */ {
         connectInfo.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         connectInfo.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-        connectInfo.put("partitioner.class", "wr1ttenyu.f1nal.study.project.archetype.test.kafka.MyPartitioner");
+        /*connectInfo.put("partitioner.class", "wr1ttenyu.f1nal.study.project.archetype.test.kafka.MyPartitioner");*/
 
-        KafkaProducer<String, String> producer = new KafkaProducer(connectInfo);
-        for (int i = 0; i < 10; i++) {
-            producer.send(new ProducerRecord("wr1-kafka-topic-second", "wr1ttenyu", "wr1ttenyu--" + i), (recordMetadata, e) -> {
+        KafkaProducer<String, String> producer = new KafkaProducer<>(connectInfo);
+        for (int i = 0; i < 3; i++) {
+            producer.send(new ProducerRecord<>("wr1-kafka-topic-first", "wr1ttenyu-first--" + i, "wr1ttenyu--first--" + i), (recordMetadata, e) -> {
                 if (e == null) {
                     System.out.println("partition : " + recordMetadata.partition() + " offset : " + recordMetadata.offset());
                 } else {
                     e.printStackTrace();
                 }
             });
+
+            producer.send(new ProducerRecord<>("wr1-kafka-topic-second", "wr1ttenyu-second--" + i, "wr1ttenyu-second--" + i), (recordMetadata, e) -> {
+                if (e == null) {
+                    System.out.println("partition : " + recordMetadata.partition() + " offset : " + recordMetadata.offset());
+                } else {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
         // 关闭资源
